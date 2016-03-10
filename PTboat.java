25f4a3edd873eeedee9ae88252d03ffd836a129c
brentmellgren <brentmellgren@gmail.com>
2016-03-10 15:59:15 -0800

    Update PTboat.java
    Added comments

--- PTboat.java
+++ PTboat.java
@@ -2,12 +2,14 @@
 public class PTboat extends Ships {
 
 	@Override
+	// Function returns the name of the ship.
 	public String getName() {
 		String shipName = "PTboat";
 		return shipName;
 	}
 
 	@Override
+	// Function returns the size of the ship.
 	public int getPegCount() {
 		int shipSize = 2;
 		return shipSize;
