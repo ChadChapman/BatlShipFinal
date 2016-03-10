48cd9d50d966707bb6f4861229a9289e98d291f7
brentmellgren <brentmellgren@gmail.com>
2016-03-10 15:37:25 -0800

    Update Destroyer.java
    Added comments

--- Destroyer.java
+++ Destroyer.java
@@ -2,12 +2,14 @@
 public class Destroyer extends Ships {
 
 	@Override
+	// Function returns the name of the ship.
 	public String getName() {
 		String shipName = "Destroyer";
 		return shipName;
 	}
 
 	@Override
+	// Function returns the size of the ship.
 	public int getPegCount() {
 		int shipSize = 3;
 		return shipSize;
